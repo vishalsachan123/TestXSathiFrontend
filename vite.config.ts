6acab8d904783e8ff3511fd6ce2bce5dfd7a4564
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2026-04-06 06:40:36 +0530

    Configure build output directory in Vite
    Add build output directory configuration to Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
 });
